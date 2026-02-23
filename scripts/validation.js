d0fec00e804da199255b73280b4b8bab812e6464
JDavidson <jd05301991@gmail.com>
2026-02-23 10:57:18 -0500

    Spots Stage 9 Part 2 Question

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,20 +2,32 @@ const showInputError = (formEl, inputEl, errorMsg) => {
     const errorMsgID = inputEl.id + "-error";
     const errorMsgEl = document.querySelector("#" + errorMsgID);
     errorMsgEl.textContent = errorMsg;
+    inputEl.classlist.add("modal__input_state_error");
+};
+
+const hideInputError = (formEl, inputEl) => {
+    const errorMsgID = inputEl.id + "-error";
+    const errorMsgEl = document.querySelector("#" + errorMsgID);
+    errorMsgEl.textContent = "";
+    inputEl.classlist.remove("modal__input_state_error");
 };
 
 const checkInputValidity = (formEl, inputEl) => {
     if (!inputEl.validity.valid) {
         showInputError(formEl, inputEl, inputEl.validationMessage);
+    } else {
+        hideInputError(formElement, inputElement);
+        const errorMsgEl = document.querySelector("#" + inputEl.id + "-error");
+        errorMsg.textContent = "";
     }
 };
 
 const setEventListners = (formEl) => {
-    const inputList = Array.form(formEl.querySelectorAll(".modal__input"));
+    const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
     const buttonElement = formEl.querySelector(".modal__button");
 
     inputList.forEach((inputElement) => {
-        inputElement.addEventListner("input", function () {
+        inputElement.addEventListener("input", function () {
             checkInputValidity(formEl, inputElement);
             toggleButtonState(inputList, buttonElement);
         });
@@ -29,4 +41,4 @@ const enableValidation = () => {
     });
 };
 
-enableValidation;
+enableValidation();
